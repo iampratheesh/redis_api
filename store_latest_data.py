a67187ed900efb8114f39cd4e54791bc29e4535d
Pratheesh Shivaprasad <pratheesh.27998@gmail.com>
2022-08-06 18:41:57 +0530

    Store latest data with device id as key in redis cache

---
+++ store_latest_data.py
@@ -0,0 +1,17 @@
+import json
+import redis
+import pandas as pd
+
+
+def parse_and_cache_data():
+    redis_client = redis.Redis(host="localhost", port=6379, db=0)
+    data = pd.read_csv("data/raw_data.csv")
+    data = data.sort_values('sts')
+    for _, series in data.iterrows():
+        row = series.to_dict()
+        device_id = row.pop('device_fk_id')
+        redis_client.set(device_id, json.dumps(row))
+
+
+if __name__ == "__main__":
+    parse_and_cache_data()
